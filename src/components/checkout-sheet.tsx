caf2fcfa9c967cca94ef0c399a90f520a0b8c572
Nahidur Zaman Tushar <nahidurzaman1903@gmail.com>
2025-08-16 20:56:21 +0000

    Confirm Your Order You're just a few steps away. order করে ফেলো!
    this p

--- src/components/checkout-sheet.tsx
+++ src/components/checkout-sheet.tsx
@@ -29,41 +29,41 @@ export default function CheckoutSheet({ product, isOpen, onOpenChange }: Checkou
   return (
     <Sheet open={isOpen} onOpenChange={onOpenChange}>
       <SheetContent className="w-full sm:max-w-lg p-0 flex flex-col">
-        <SheetHeader className="p-6 pb-4">
+        <SheetHeader className="p-6 pb-2">
           <SheetTitle className="font-headline text-2xl">Confirm Your Order</SheetTitle>
           <SheetDescription>
             You're just a few steps away. order করে ফেলো!
           </SheetDescription>
-           <div className="relative w-full pt-4">
-            <Carousel className="w-full">
-              <CarouselContent>
-                {product.images.map((image, index) => (
-                  <CarouselItem key={index}>
-                    <div className="relative h-48 w-full">
-                      <Image
-                        src={image}
-                        alt={`${product.name} image ${index + 1}`}
-                        fill
-                        className="object-cover rounded-md"
-                        data-ai-hint={product.dataAiHint}
-                      />
-                    </div>
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-              {product.images.length > 1 && (
-                <>
-                  <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2" />
-                  <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2" />
-                </>
-              )}
-            </Carousel>
-          </div>
         </SheetHeader>
         <ScrollArea className="flex-grow">
-            <div className="p-6 pt-0">
-                <CheckoutForm product={product} onSuccess={handleSuccess} />
+          <div className="p-6 pt-4">
+             <div className="relative w-full mb-6">
+              <Carousel className="w-full">
+                <CarouselContent>
+                  {product.images.map((image, index) => (
+                    <CarouselItem key={index}>
+                      <div className="relative h-48 w-full">
+                        <Image
+                          src={image}
+                          alt={`${product.name} image ${index + 1}`}
+                          fill
+                          className="object-cover rounded-md"
+                          data-ai-hint={product.dataAiHint}
+                        />
+                      </div>
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                {product.images.length > 1 && (
+                  <>
+                    <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2" />
+                    <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2" />
+                  </>
+                )}
+              </Carousel>
             </div>
+            <CheckoutForm product={product} onSuccess={handleSuccess} />
+          </div>
         </ScrollArea>
       </SheetContent>
     </Sheet>
